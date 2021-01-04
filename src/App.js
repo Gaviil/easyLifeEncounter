c9b91137c0185cf73e2b4ed69b0e238f97642b02
Quentin <quentinb@jimmyfairly.com>
2021-01-04 11:58:37 +0100

    init project

---
+++ src/App.js
@@ -0,0 +1,204 @@
+/* eslint-disable default-case */
+import React from 'react';
+import ListEncounter from './ListEncounter.jsx'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:'',
+      heathPoint: 1,
+      encounter: JSON.parse(localStorage.getItem('listEncounter')) || [],
+      color: [
+        '#ff3064', '#16d3d4', '#1bc97f', '#fec925', '#eb32bc', '#5087ec'
+      ]
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleClick(e) {
+    let listEncounter = this.state.encounter;
+    listEncounter.push({
+      name:this.state.name,
+      maxHealth:this.state.heathPoint,
+      currentHealth:this.state.heathPoint
+    });
+
+    this.setState({
+      name:'',
+      heathPoint: 1,
+      encounter: listEncounter,
+    }, () => {
+      this.saveDataLocal(listEncounter);
+    })
+    e.preventDefault();
+  }
+
+  updateHealth = (i, value, type) => {
+    const updateHealth = this.state.encounter;
+    switch (type) {
+      case 'up':
+        if(parseInt(updateHealth[i].currentHealth) + value <= updateHealth[i].maxHealth) {
+          updateHealth[i].currentHealth = JSON.stringify(parseInt(updateHealth[i].currentHealth) + value);
+        }
+        this.setState({
+          encounter: updateHealth
+        },() => {
+          this.saveDataLocal(updateHealth);
+        });
+        break;
+      case 'lower':
+        if(parseInt(updateHealth[i].currentHealth) - value >= 0) {
+          updateHealth[i].currentHealth = JSON.stringify(parseInt(updateHealth[i].currentHealth) - value);
+        }
+        this.setState({
+          encounter: updateHealth
+        },() => {
+          this.saveDataLocal(updateHealth);
+        });
+        break;
+    }
+  }
+
+  saveDataLocal = (listEncounter) => {
+    localStorage.setItem('listEncounter', JSON.stringify(listEncounter))
+  }
+
+  selectColorBar = (i) => {
+    if (i >= this.state.color.length) {
+      return this.state.color[i % this.state.color.length];
+    }
+    return this.state.color[i];
+  }
+
+  clearAll = () => {
+    this.setState({
+      encounter: [],
+    }, () => {
+      this.saveDataLocal([])
+    })
+  }
+
+
+  render() {
+    document.body.style.backgroundColor = "#002245";
+
+    return (
+      <div style={{padding: 20, display: 'flex', flexDirection: 'column'}}>
+        <div>
+          <form 
+            style={{
+              padding: 0,
+              display: 'flex',
+              flexDirection: 'column',
+              backgroundColor:'#002245'
+            }}>
+            <label style={{color: '#fff'}}>
+              Name : 
+              <input
+                name="name"
+                type="text"
+                style={{
+                  marginTop: 10,
+                  borderWidth: 0,
+                  borderBottomWidth: 1,
+                  outline: 'none',
+                  marginInline: 10,
+                  paddingInline: 5,
+                  borderColor: '#fff',
+                  backgroundColor: '#002245',
+                  color: '#fff'
+                }}
+                value={this.state.name}
+                onChange={this.handleInputChange} />
+            </label>
+            <label style={{color: '#fff'}}>
+              HealthPoint :
+              <input
+                min={1}
+                name="heathPoint"
+                type="number"
+                style={{
+                  marginTop: 20,
+                  width: 105,
+                  borderWidth: 0,
+                  borderBottomWidth: 1,
+                  outline: 'none',
+                  marginInline: 10,
+                  paddingInline: 5,
+                  borderColor: '#fff',
+                  backgroundColor: '#002245',
+                  color: '#fff'
+                }}
+                value={this.state.heathPoint}
+                onChange={this.handleInputChange} />
+            </label>
+          <button 
+            onClick={this.handleClick}
+            style={{
+              marginTop: 10,
+              backgroundColor: '#002245',
+              width: 100,
+              borderWidth: 0,
+              color: '#16d3d4',
+              fontWeight: 'bold',
+              fontSize: 18,
+              outline: 'none',
+            }}
+          >
+              Create
+            </button>
+        </form>
+        <button
+          onClick={() => this.clearAll()}
+          style={{
+              marginTop: 5,
+              backgroundColor: '#002245',
+              borderWidth: 0,
+              width: 100,
+              color: '#fec925',
+              fontWeight: 'bold',
+              fontSize: 18,
+              outline: 'none',
+            }}
+        >
+          Clear all
+        </button>
+        </div>
+        <div>
+          {this.state.encounter.map((value,index) => (
+             <ListEncounter
+              key={index}
+              encounter={value}
+              index={index}
+              color={this.selectColorBar(index)}
+              updateHealth={(i, value, type) => this.updateHealth(i, value, type)}
+              remove={(i) => {
+                const listEncounter = this.state.encounter;
+                listEncounter.splice(i,1);
+                this.setState({
+                  encounter: listEncounter
+                },() => {
+                  this.saveDataLocal(listEncounter);
+                })
+              }}
+            />
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/ListEncounter.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+
+class listEncounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hp: null
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    const {encounter} = this.props;
+    const currentPercent = (encounter.currentHealth * 100) / encounter.maxHealth; 
+    return (
+      <div style={{flex: 1, marginTop: 20}}>
+        <p style={{color: '#fff', marginTop: 2}}>{encounter.name} </p>
+        <p style={{color: '#fff', marginTop: 2}}>{encounter.currentHealth} / {encounter.maxHealth}
+          <input
+            style={{
+              borderWidth: 0,
+              borderBottomWidth: 1,
+              outline: 'none',
+              marginInline: 10,
+              paddingInline: 5,
+              borderColor: '#fff',
+              backgroundColor: '#002245',
+              color: '#fff'
+            }}
+            min={1}
+            name="hp"
+            type="number"
+            value={this.state.hp}
+            onChange={this.handleInputChange} />
+          <button
+            style={{
+              backgroundColor: '#002245',
+              borderWidth: 0,
+              color: '#ff3064',
+              fontWeight: 'bold',
+              fontSize: 18,
+              outline: 'none',
+            }}
+            onClick={() => {
+            this.props.updateHealth(this.props.index, parseInt(this.state.hp), 'lower');
+            }}
+          >
+            Damage
+          </button>
+          <button
+            style={{
+              backgroundColor: '#002245',
+              borderWidth: 0,
+              color: '#1bc97f',
+              fontWeight: 'bold',
+              fontSize: 18,
+              outline: 'none',
+            }}
+            onClick={() => {
+            this.props.updateHealth(this.props.index, parseInt(this.state.hp), 'up');
+          }}
+          >
+            Heal
+          </button>
+
+          <button
+            style={{
+              marginLeft: 150,
+              backgroundColor: '#002245',
+              borderWidth: 0,
+              color: '#5087ec',
+              fontWeight: 'bold',
+              fontSize: 18,
+              outline: 'none',
+            }}
+            onClick={() => {
+            this.props.remove(this.props.index);
+          }}
+          >
+            Remove
+          </button>
+        </p>
+        <div
+          style={{
+            position: 'relative',
+            width: 300,
+            height: 30,
+            backgroundColor: 'grey'
+          }}>
+            <div
+            style={{
+              width: `${currentPercent}%`,
+              height: '100%',
+              backgroundColor: this.props.color || '#16d3d4'
+            }}/>
+        
+          <div
+            style={{
+              width: '100%',
+              height: '100%',
+              position:'absolute',
+              top: 0
+            }}>
+            <button
+              style={{
+                width: '50%',
+                height:'100%',
+                opacity: 0
+              }}
+              onClick={() => this.props.updateHealth(this.props.index, 1, 'lower')}
+              >
+              </button>
+            <button
+              style={{
+                width: '50%',
+                height:'100%',
+                opacity: 0
+              }}
+              onClick={() => this.props.updateHealth(this.props.index, 1, 'up')}
+              >
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default listEncounter;
